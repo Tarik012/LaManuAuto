e62b8d05fe8500ffe255b73f395259779bbd9324
Akonedev <93835972+Akonedev@users.noreply.github.com>
2022-06-02 09:51:14 +0200

    Akone ajout template complet

---
+++ LaManuAuto/Services/DataService.cs
@@ -0,0 +1,27 @@
+﻿using LaManuAuto.Data;
+using LaManuAuto.Models;
+
+namespace LaManuAuto.Services
+{
+    public class DataService : IDataService
+    {
+       private readonly LAMANU_AUTOContext _Context;
+
+        public DataService(LAMANU_AUTOContext context)
+        {
+            _Context = context;
+        }
+        public List<Tutoriel> GetTutorials(string tuto)
+        {
+            var tutoriels = _Context.Tutoriels
+                .Where(x => x.Id == 1);
+            var users = _Context.Users
+                .Where(x => x.Email.EndsWith("gmail.com"));
+       
+        if(users.Any())
+            return tutoriels.ToList();
+        return new List<Tutoriel>();
+        }
+
+    }
+}
