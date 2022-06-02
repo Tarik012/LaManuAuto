f9456d1590211fdc660887521ad6153026b3be04
Akonedev <93835972+Akonedev@users.noreply.github.com>
2022-06-02 12:02:46 +0200

    Ajout initial template 0206 D

---
+++ LaManuAuto/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LaManuAuto.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<IdentityUser> _userManager;
+
+        public UserController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        //[Authorize(Roles ="Admin")]
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        
+       // [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View(new IdentityRole());  
+        }
+
+
+        
+        //[Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(IdentityRole role)
+        {
+            await _roleManager.CreateAsync(role);
+            return View();
+        }
+
+        public async Task<IActionResult> GetMyRoles()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            var user = await _userManager.FindByNameAsync(userEmail);
+            var roles = await _userManager.GetRolesAsync(user);
+            return View(roles);
+        }
+    }
+}
